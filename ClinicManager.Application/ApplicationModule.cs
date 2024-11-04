86e56b5cffee8c2634457feeddddf031c849759e
Caio Cesar <caiocsmeneghelli@outlook.com>
2024-11-03 22:11:57 -0300

    Add: applicationModule com erro.

---
+++ ClinicManager.Application/ApplicationModule.cs
@@ -0,0 +1,34 @@
+﻿using ClinicManager.Application.Commands.Doctors.CreateDoctor;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddMediator(); 
+                //.AddValidation();
+            //.AddAutoMapper()
+            return services;
+        }
+
+        private static IServiceCollection AddMediator(this IServiceCollection service)
+        {
+            service.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateDoctorCommand).Assembly));
+            return service;
+        }
+
+        //private static IServiceCollection AddValidation(this IServiceCollection service)
+        //{
+        //    service.AddValidatorsFromAssemblyContaining<CreateDoctorCommandValidator>();
+        //    return service;
+        //}
+    }
+}
