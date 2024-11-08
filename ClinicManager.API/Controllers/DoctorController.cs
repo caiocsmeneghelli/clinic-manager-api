945e88b68d1a90c179e505b6f00dbf6c0b428137
Caio Cesar <caiocsmeneghelli@outlook.com>
2024-11-07 22:14:04 -0300

    Feat: criado Doctors Controller.

---
+++ ClinicManager.API/Controllers/DoctorController.cs
@@ -0,0 +1,49 @@
+﻿using ClinicManager.Application.Commands.Doctors.CreateDoctor;
+using ClinicManager.Application.Queries.Doctors.GetAll;
+using ClinicManager.Application.Queries.Doctors.GetDoctorById;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllDoctorsQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetDoctorByIdQuery(id);
+            var result = await _mediator.Send(query);
+            if(result.IsSuccess == false)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateDoctorCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (result.IsSuccess == false)
+            {
+                return BadRequest(result);
+            }
+
+            return Created();
+        }
+    }
+}
