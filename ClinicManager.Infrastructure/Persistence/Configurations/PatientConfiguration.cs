df8a506774e7adcbf64b8798a16a3fc6040f07ed
Caio Cesar <caiocsmeneghelli@outlook.com>
2024-11-20 22:44:47 -0300

    feat: add patient e user configuration

---
+++ ClinicManager.Infrastructure/Persistence/Configurations/PatientConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ClinicManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Infrastructure.Persistence.Configurations
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.HasKey(reg => reg.Id);
+
+            builder.HasOne(reg => reg.User)
+                .WithMany()
+                .HasForeignKey(reg => reg.IdUser);
+        }
+    }
+}
--- ClinicManager.Infrastructure/Persistence/Context/ClinicManagerDbContext.cs
+++ ClinicManager.Infrastructure/Persistence/Context/ClinicManagerDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,9 @@ namespace ClinicManager.Infrastructure.Persistence.Context
         public ClinicManagerDbContext(DbContextOptions options) : base(options)
         { }
 
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
