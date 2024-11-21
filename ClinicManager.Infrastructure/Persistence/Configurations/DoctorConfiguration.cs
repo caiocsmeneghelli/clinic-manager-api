34e56798f0dac07980c4c5707d99d3b41a3f4e8a
Caio Cesar <caiocsmeneghelli@outlook.com>
2024-11-20 22:40:11 -0300

    Feat: add dbManager e dbContext.

---
+++ ClinicManager.Infrastructure/Persistence/Configurations/DoctorConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ClinicManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Infrastructure.Persistence.Configurations
+{
+    public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+    {
+        public void Configure(EntityTypeBuilder<Doctor> builder)
+        {
+            builder.HasKey(reg => reg.Id);
+
+            builder.HasOne(reg => reg.User)
+                .WithMany()
+                .HasForeignKey(reg => reg.IdUser);
+        }
+    }
+}
---
+++ ClinicManager.Infrastructure/Persistence/Context/ClinicManagerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ClinicManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Infrastructure.Persistence.Context
+{
+    public class ClinicManagerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        public ClinicManagerDbContext(DbContextOptions options) : base(options)
+        { }
+
+
+    }
+}
