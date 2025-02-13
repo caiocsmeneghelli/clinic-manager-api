581e9554417c3061cccdbc993a8eafc60175e35e
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-02-12 22:21:02 -0300

    feat: add novo metodo para cancelamento de medico, sem finalizar.

---
+++ ClinicManager.Application/Commands/Doctors/Cancel/CancelDoctorCommand.cs
@@ -0,0 +1,20 @@
+﻿using ClinicManager.Application.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Application.Commands.Doctors.Cancel
+{
+    public class CancelDoctorCommand : IRequest<Result>
+    {
+        public int IdDoctor { get; private set; }
+
+        public CancelDoctorCommand(int idDoctor)
+        {
+            IdDoctor = idDoctor;
+        }
+    }
+}
---
+++ ClinicManager.Application/Commands/Doctors/Cancel/CreateDoctorCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using ClinicManager.Application.Results;
+using ClinicManager.Domain.UnitOfWork;
+using MediatR;
+
+namespace ClinicManager.Application.Commands.Doctors.Cancel
+{
+    public class CancelDoctorCommandHandler : IRequestHandler<CancelDoctorCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CancelDoctorCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Result> Handle(CancelDoctorCommand request, CancellationToken cancellationToken)
+        {
+            var doctor = await _unitOfWork.Doctors.GetByIdAsync(request.IdDoctor);
+            if(doctor == null) { return Result.NotFound("Médico não encontrado."); }
+
+            doctor.Cancel();
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success();
+        }
+    }
+}
